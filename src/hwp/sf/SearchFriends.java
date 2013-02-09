e1d41b4f85eb7e1f70a83573df5b742020fbbc17
Weipeng He <heweipeng@gmail.com>
2013-02-09 15:14:29 +0100

    Initial Commit

---
+++ src/hwp/sf/SearchFriends.java
@@ -0,0 +1,13 @@
+package hwp.sf;
+
+/**
+ * Game for Search Friends (Zhao Peng You) !
+ * @author Weipeng He
+ * @version 1.0
+ */
+public class SearchFriends {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
+
